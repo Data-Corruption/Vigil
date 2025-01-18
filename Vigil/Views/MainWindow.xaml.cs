d210250dc9dd690ecbc4f51a3940a5dc7e6181c8
Matthew Pombo <datacorruption10@gmail.com>
2025-01-18 12:43:41 -0800

    main window styling

--- Vigil/Views/MainWindow.xaml.cs
+++ Vigil/Views/MainWindow.xaml.cs
@@ -5,44 +5,49 @@ using Vigil.Hardware;
 
 namespace Vigil.Views
 {
-    public partial class MainWindow : Window
+  public partial class MainWindow : Window
+  {
+    private readonly HardwareMonitor _hardwareMonitor;
+    private readonly DispatcherTimer _timer;
+    private SettingsWindow? _settingsWindow;
+
+    public MainWindow(HardwareMonitor hardwareMonitor)
     {
-        private readonly HardwareMonitor _hardwareMonitor;
-        private readonly DispatcherTimer _timer;
-        private SettingsWindow? _settingsWindow;
+      InitializeComponent();
+      _hardwareMonitor = hardwareMonitor;
 
-        public MainWindow(HardwareMonitor hardwareMonitor)
-        {
-            InitializeComponent();
-            _hardwareMonitor = hardwareMonitor;
-
-            // Initialize and start the timer to update UI every second
-            _timer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(1)
-            };
-            _timer.Tick += Draw;
-            _timer.Start();
-        }
-
-        private void Draw(object sender, EventArgs e)
-        {
-            OutputTextBox.Text = _hardwareMonitor.GetLatestData();
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
-            {
-                if (_settingsWindow != null)
-                {
-                    return;
-                }
-                _settingsWindow = new SettingsWindow(_hardwareMonitor);
-                _settingsWindow.Owner = this;
-                _settingsWindow.Closed += (s, e) => _settingsWindow = null;
-                _settingsWindow.Show();
-            }
-        }
+      // Initialize and start the timer to update UI every second
+      _timer = new DispatcherTimer
+      {
+        Interval = TimeSpan.FromSeconds(1)
+      };
+      _timer.Tick += Draw;
+      _timer.Start();
     }
+
+    private void Draw(object sender, EventArgs e)
+    {
+      // OutputTextBox.Text = _hardwareMonitor.GetLatestData();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+      {
+        if (_settingsWindow != null)
+        {
+          return;
+        }
+        _settingsWindow = new SettingsWindow(_hardwareMonitor);
+        _settingsWindow.Owner = this;
+        _settingsWindow.Closed += (s, e) => _settingsWindow = null;
+        _settingsWindow.Show();
+      }
+      else
+      {
+        if (e.ChangedButton == MouseButton.Left)
+          this.DragMove();
+      }
+    }
+  }
 }
